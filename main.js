8c1eccf6851d8d548f022eeee07eecdf17128a16
Ana Gabriela <anagabrieladev@gmail.com>
2021-09-27 18:18:27 -0300

    feat: implements repo from github

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
+async function getRepositories() {
+    const repositories = await fetch("https://api.github.com/users/AnaGabrielaDev/repos")
+        .then(response => response.json());
+
+    return repositories;
+}
+
+async function writeRepo() {
+    const repositoriesDiv = document.getElementById('repositories');
+    const repositories = await getRepositories();
+
+    repositories.forEach(repo => {
+        const div = document.createElement('div');
+        div.className = "card";
+
+        //criar link de referencia + titulo
+        const a = document.createElement('a');
+        a.href = repo.html_url;
+        const title = document.createElement('h3');
+        title.innerHTML = repo.name
+        a.appendChild(title);
+
+        const p = document.createElement('p');
+        p.innerHTML = repo.description !== null ? repo.description : "";
+
+        div.append(a);
+        div.append(p);
+
+        repositoriesDiv.appendChild(div);
+    })
+}
+writeRepo();
+
 function typeWrite(elemento) {
     const textoArray = elemento.innerHTML.split('');
     elemento.innerHTML = ''
